5342d0f2a4dbee40949dc2e87d1d169e94823270
sfletche <sfletche@gmail.com>
2015-09-26 22:20:16 -0700

    formatting gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
-"use strict";
-
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
     watch = require('gulp-watch'),
-    plumber = require('gulp-plumber'),
+    plumber = require('gulp-plumber');
 
 path = {
-    src: {
-        js: 'src/app.js'
-    },
-    dist: {
-        js: "dist/"
-    }
+  src: {
+    js: 'src/app.js'
+  },
+  dist: {
+    js: "dist/"
+  }
 };
 
 gulp.task('6to5', function () {
-    gulp.src(path.src.js)
-        .pipe(plumber())
-        .pipe(babel())
-        .pipe(plumber.stop())
-        .pipe(gulp.dest(path.dist.js));
+  gulp.src(path.src.js)
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.dist.js));
 });
 
 gulp.task('watch', ['6to5'], function (){
-    gulp.watch([path.src.js], [babel]);
+  gulp.watch([path.src.js], [babel]);
 });
 
 gulp.task('default', ['watch']);
